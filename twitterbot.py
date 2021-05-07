89dee7919234f542a1d057acd19ee235aeed87ee
Sakshi Kedar <52277353+sakshikedar@users.noreply.github.com>
2021-05-07 20:03:05 +0530

    Add files via upload

---
+++ twitterbot.py
@@ -0,0 +1,30 @@
+import tweepy as twitter
+import secrets
+import datetime
+import time
+
+auth = twitter.OAuthHandler(secrets.API_KEY,secrets.API_SCERET_KEY)
+auth.set_access_token(secrets.ACCESS_TOKEN,secrets.SECRET_ACCESS_TOKEN)
+api = twitter.API(auth)
+
+def bot(hashtags):
+    while True:
+        print(datetime.datetime.now())
+
+        for hashtag in hashtags:
+            for tweet in twitter.Cursor(api.search, q=hashtag, rpp=10).items(5):
+                try:
+                    id = dict(tweet._json)['id']
+                    text = dict(tweet._json)['text']
+
+                    api.retweet(id)
+                    api.create_favorite(id)
+
+                    print("Tweet ID:",id)
+                    print("Tweet Text:",text)
+                except twitter.tweepError as e:
+                    print(e.reason)
+
+            time.sleep(10)
+
+bot(['Covid19Aid'])
